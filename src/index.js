3b0e32f593b041755fc45b33ab73596203cde403
Dolix7 <51421090+Dolix7@users.noreply.github.com>
2023-03-24 00:20:09 +0100

    delete dist, and move icon to the racine

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const createWindow = () => {
   const mainWindow = new BrowserWindow({
     width: 350,
     height: 300,
-    icon: path.join(__dirname, '/dist/assets/imgs/passgenlogo.png'),
+    icon: path.join(__dirname, 'passgenlogo.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
